8d302a99b5b97172065b4b4b26528b73cc094208
ghjingrui0564 <41656578+ghjingrui0564@users.noreply.github.com>
2018-08-03 00:17:16 +0800

    Create SavingsAccount.h

---
+++ bank account1.0/SavingsAccount.h
@@ -0,0 +1,21 @@
+//SavingsAccount.h
+#ifndef _SAVINGSACCOUNT_H_
+#define _SAVINGSACCOUNT_H_
+#include"date.h"
+#include"accumulator.h"
+#include"account.h"
+#include<string>
+class SavingsAccount:public Account{
+private:
+	Accumulator acc;					//an accumulator for calculating interest
+	double rate;						//the annual interest on deposits
+public:
+	//constructor
+	SavingsAccount(Date &date,const std::string &id,double rate);
+	double getRate()const {return rate;}
+	//action
+	void deposit (const Date &date,double amount,const std::string &desc);
+	void withdraw(const Date &date,double amount,const std::string &desc);
+	void settle(const Date &date);		//calculate interest,it is called once a year on January 1st
+};
+#endif //_SAVINGSACCOUNT_H_
