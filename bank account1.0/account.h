adbccfcd830903576a0abffff576d5afd7a6ff16
ghjingrui0564 <41656578+ghjingrui0564@users.noreply.github.com>
2018-08-03 00:15:39 +0800

    Update account.h

--- bank account1.0/account.h
+++ bank account1.0/account.h
@@ -2,36 +2,24 @@
 #ifndef _ACCOUNT_H_
 #define _ACCOUNT_H_
 #include"date.h"
+#include"accumulator.h"
 #include<string>
-class SavingsAccount{
+class Account{
 private:
 	std::string id;
 	double balance;
-	double rate;							//the annual interest rate on deposits
-	Date lastDate;							//date of the last time the balance was changed
-	double accumulation;					//the sum of the balances accumulated on a daily basis
-	static double total;					//total amount of all accounts
+	static double total;
+protected:
+	//a constructor called by a derived class
+	Account(const Date &date,const sht::string &id);
 	void record(const Date &date,double amount,const std::string &desc);
-	//
+	//report error message 
 	void error(const std::string &msg) const;
-	//
-	double accumulate(const Date &date) const{
-		return accumulation+balance*date.distance(lastDate);
-	}
 public:
-	//the constructor
-	SavingsAccount(const Date &date,const std::string &id,double rate);
-	const std::string &getId() const {return id;}
-	double getBalance() const{return balance}
-	double fetRate() const {return rate;}
-	static double getTotal(){return total;}
-	//deposit money
-	void deposit(const Date &date,double amount,const std::string &desc);
-	//withdraw money
-	void withdraw(const Date &date,double amount,const std::string &desc);
-	//settle the interest,this function is called on January 1st every year
-	void settle(const Date &date);
-	//display account information
+	const std::string &getId(){return id;}
+	double getBalance()const {return balance;}
+	static double total(){return total;}
 	void show() const;
 };
-#endif
+#endif //_ACCOUNT_H_
+
