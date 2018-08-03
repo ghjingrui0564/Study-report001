f072fd8fcd58195a94db007adb67db4c006c7c21
ghjingrui0564 <41656578+ghjingrui0564@users.noreply.github.com>
2018-08-04 00:31:07 +0800

    Create CreditAccount.cpp

---
+++ bank account1.0/CreditAccount.cpp
@@ -0,0 +1,34 @@
+//CreditAccount.cpp
+#include"CreditAccount.h"
+#include<cmath>
+#include<iostream>
+
+using namespace std;
+
+CreditAccount::CreditAccount(const Date& date,const string &id,double credit,double rate,double fee)
+:Account(date,id),credit(credit),rate(rate),fee(fee),acc(date,0){}
+//CreditAccount(const Date &date,const std::string &id,double rate,double fee);
+
+void CreditAccount::deposit(const Date &date,double amount,const string &desc){
+	record(date,amount,desc);
+	acc.change(date,getDebt());
+}
+void CreditAccount::withdraw(const Date &date,double amount,const string &desc){
+	if(amount-getBalance()>credit){
+		error("not enough credit");
+	}else{
+		record(date,-amount,desc);
+		acc.change(date,getDebt());
+	}
+}
+void CreditAccount::settle(const Date &date){
+	double interest=acc.getSum(date)*rate;
+	if(interest!=0) record(date,interest,"interest");
+	if(date.getMonth()==1)
+		record(date,-fee,"annual fee");
+	acc.reset(date,getDebt());
+}
+void CreditAccount::show() const{
+	Account::show();
+	cout<<"\tAvailable credit:"<<getAvailableCredit();
+}
