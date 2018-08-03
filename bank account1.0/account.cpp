ff1f1590c1e5e679658a13b0cfd4114e9dda8136
ghjingrui0564 <41656578+ghjingrui0564@users.noreply.github.com>
2018-08-04 00:29:48 +0800

    Create account.cpp

---
+++ bank account1.0/account.cpp
@@ -0,0 +1,24 @@
+//account.cpp
+#include"account.h"
+#include<cmath>
+#include<iostream>
+using namespace std;
+double Account::total=0;
+Account::Account(const Date &date,const string &id)
+:id(id),balance(0){
+	date.show();
+	cout<<"\t#"<<id<<"created"<<endl;
+}
+void Account::record(const Date &date,double amount,const string &desc){
+	amount=floor(amount*100+0.5)/100;
+	balance+=amount;
+	total+=amount;
+	date.show();
+	cout<<"\t#"<<id<<"\t#"<<amount<<"\t"<<balance<<"\t#"<<desc<<endl;
+}
+void Account::show() const{
+	cout<<id<<"\tBalance:"<<balance;
+}
+void Account::error(const string &msg) const{
+	cout<<"error(#"<<id<<"):"<<msg<<endl;
+}
