98517f2b9c62b841271caa2c42c65fd7a4440145
ghjingrui0564 <41656578+ghjingrui0564@users.noreply.github.com>
2018-08-09 00:54:03 +0800

    Update accumulator.h

--- bank account1.0/accumulator.h
+++ bank account1.0/accumulator.h
@@ -1,31 +1,16 @@
-//accumulator.h
-#ifndef __ACCMULATOR_H__
-#define __ACCMULATOR_H__
+#endif                   //__ACCUMULATOR_H__
+#ifndef __ACCUMULATOR_H__
+#define __ACCUMULATOR_H__
 #include"date.h"
-class Accumulator{
+class Accumulator {
 private:
 	Date lastDate;
-	double value;		//current value
-	double sum;			//the sum of values by day
+	double value;
+	double sum;
 public:
-	//constructor,date is the beginning date of accumulation
-	//value is the initial value
-	Accumulator(const Date &date,double value):lastDate(date),value(value),sum(0){}
-	//get the cumulative result of the date
-	double getSum(const Date &date) const{
-		return sum+value*date.distance(lastDate);
-	}
-	//change value in date to value
-	void change(const Date &date,double value){
-		sum=getSum(date);
-		lastDate=date;
-		this->value=value;
-	}
-	//initialization
-	void reset(const Date &date,double value){
-		lastDate=date;
-		this->value=value;
-		sum=0;
+	double getSum(const Date &date)const {
+		return sum + value * (date - lastDate);
 	}
 };
-#endif                   //__ACCUMULATOR_H__
+#endif // !__ACCUMULATOR_H__
+
