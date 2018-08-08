a1ce79fd5516369e01802db50dae4f22d97e7d06
ghjingrui0564 <41656578+ghjingrui0564@users.noreply.github.com>
2018-08-09 00:34:20 +0800

    Update date.h

--- bank account1.0/date.h
+++ bank account1.0/date.h
@@ -1,24 +1,24 @@
 //date.h
-#ifndef __DATE_H__
-#define __DATE_H__
-class Date{
+#ifndef  __DATE_H__
+#define  __DATE_H__
+class Date {
 private:
 	int year;
 	int month;
 	int day;
-	int totalDays;							//calculations begin on January 1st
+	int totalDays;
 public:
-	Date(int year,int month,int day);
-	int getYear() const{return year;}
-	int getMonth() const{return month;}
-	int getDay() const{return day;}
-	int getMaxDay() const;					//get the number of days of the month
-	bool isLeapYear() const{				//determine whether it is a leap year
-		return year%4==0 && year%100!=0 || year%400==0;
+	Date(int year, int month, int day);
+	int getYear()const { return year; }
+	int getMonth()const { return month; }
+	int getDay()const { return day; }
+	int getMaxDay()const;
+	bool isLeapYear()const {
+		return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
 	}
-	void show() const;						//output current date
-	int distance(const Date &date) const{
-		return totalDays-date.totalDays;
+	void show()const;
+	int operator-(const Date& date)const {
+		return totalDays - date.totalDays;
 	}
 };
-#endif //__DATE_H__
+#endif // !_DATE_H_
