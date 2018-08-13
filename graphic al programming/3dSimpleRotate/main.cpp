f81494db33ed058960365fbbe9c7f94b10c59ec8
ghjingrui0564 <41656578+ghjingrui0564@users.noreply.github.com>
2018-08-14 00:32:25 +0800

    Update main.cpp

--- graphic al programming/3dSimpleRotate/main.cpp
+++ graphic al programming/3dSimpleRotate/main.cpp
@@ -54,8 +54,8 @@ int main()
 
 	//build and compile shader program
 	//---------------------------------
-	Shader ourShader("E:\Vs-Code\c++\Graphic\3dSimpleRotate\3dSimpleRotate\coordinate_system.vs", 
-		"E:\Vs-Code\c++\Graphic\3dSimpleRotate\3dSimpleRotate\coordinate_system.fs");
+	Shader ourShader("E:\\Vs-Code\\c++\\Graphic\\3dSimpleRotate\\3dSimpleRotate\\coordinate_system.vs", 
+		"E:\\Vs-Code\\c++\\Graphic\\3dSimpleRotate\\3dSimpleRotate\\coordinate_system.fs");
 
 	//set up vertex data ande configure vertex attributes
 	//----------------------------------------------------
@@ -135,7 +135,7 @@ int main()
 	//load image,create texture and generate mipmaps
 	int width, height, nrChannels;
 	stbi_set_flip_vertically_on_load(true);//tell stb_image.h to flip loaded texture's on the y-axis.
-	unsigned char *data = stbi_load(FileSystem::getPath("E:\Download\OperaDownload\container.jpg").c_str(),&width, &height, &nrChannels, 0);
+	unsigned char *data = stbi_load(("E:\\Download\\OperaDownload\\container.jpg"),&width, &height, &nrChannels, 0);
 	if (data)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -158,7 +158,7 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	//load image,create texture and generate mipmaps
-	data = stbi_load(FileSystem::getPath("E:\Download\OperaDownload\awesomeface.png").c_str(), &width, &height, &nrChannels, 0);
+	data = stbi_load(("E:\\Download\\OperaDownload\\awesomeface.png"), &width, &height, &nrChannels, 0);
 	if (data)
 	{
 		//note that the .png has transparency and thus an alpha channel,so make sure to tell opengl the
